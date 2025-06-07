25ea3a771e4b61656068c75b5c2f00bc0c554ab7
warubert <william.rubert@gmail.com>
2025-06-06 21:54:56 -0300

    ajustado testes de  property

--- src/domain/entities/property.test.ts
+++ src/domain/entities/property.test.ts
@@ -1,3 +1,4 @@
+import { DateRange } from '../value_objects/date_range';
 import { Property } from './property';
 
 describe("Property Entity", () => {
@@ -16,4 +17,41 @@ describe("Property Entity", () => {
         expect(property.getMaxGuests()).toBe(4);
         expect(property.getBasePricePerNight()).toBe(200);
     });
+
+    it('deve lançar um erro se nome for vazio', () => {
+        expect(() => {
+            new Property("1", "", "a", 4, 200);
+        }).toThrow('Nome é obrigatório');
+    });
+
+    it('deve lançar um erro se numero de hospedes for 0 ou negativo for vazio', () => {
+        expect(() => {
+            new Property("1", "Casa", "a", 0, 200);
+        }).toThrow('O numero maximo de hospedes deve ser maior que 0');
+    });
+
+    it('deve validar o numero máximo de hospedes', () => {
+        const property = new Property("1", "Casa", "a", 5, 200);    
+        expect(() => {
+            property.validateGuestCount(6);
+        }).toThrow('Número máximo de hospedes excedido. Máximo permitido é 5');
+    });
+
+    it('Não deve aplicar desconto para estadias menores que 7 noites', () => {
+        const property = new Property("1", "Casa", "a", 5, 200);
+        const startDate = new Date('2025-06-20');
+        const endDate = new Date('2025-06-26');
+        const dateRange = new DateRange(startDate, endDate);
+        const totalPrice = property.calculateTotalPrice(dateRange);
+        expect(totalPrice).toBe(1200);
+    })
+
+    it('Deve aplicar desconto para estadias de 7 noites ou mais', () => {
+        const property = new Property("1", "Casa", "a", 5, 200);
+        const startDate = new Date('2025-06-20');
+        const endDate = new Date('2025-06-27');
+        const dateRange = new DateRange(startDate, endDate);
+        const totalPrice = property.calculateTotalPrice(dateRange);
+        expect(totalPrice).toBe(1400*0.9);
+    })
 });
--- src/domain/entities/property.ts
+++ src/domain/entities/property.ts
@@ -1,6 +1,8 @@
+import { DateRange } from "../value_objects/date_range";
+
 export class Property {
-
-
+    
+    
     constructor(
         private id: string,
         private name: string,
@@ -8,26 +10,51 @@ export class Property {
         private maxGuests: number,
         private basePricePerNight: number,
     ) {
+        if(!name){
+            throw new Error('Nome é obrigatório');
+        }
+        if(maxGuests <= 0){
+            throw new Error('O numero maximo de hospedes deve ser maior que 0');
+        }
         this.id = id;
         this.name = name;
         this.description = description;
         this.maxGuests = maxGuests;
         this.basePricePerNight = basePricePerNight;
     }
-
+    
     getId() {
         return this.id;
     }
+
     getName() {
         return this.name;
     }
+    
     getDescription() { 
         return this.description;
     }
+    
     getMaxGuests() {
         return this.maxGuests;
     }
+    
     getBasePricePerNight() {
         return this.basePricePerNight;
+    }
+    
+    validateGuestCount(guestCount: number): void {
+        if(guestCount > this.maxGuests){
+            throw new Error('Número máximo de hospedes excedido. Máximo permitido é ' + this.maxGuests);
+        }
+    }
+    
+    calculateTotalPrice(dateRange: DateRange): number {
+        const totalNights = dateRange.getTotalNights();
+        const totalPrice = totalNights*this.basePricePerNight;
+        if(totalNights >= 7){
+            return totalPrice * 0.9;
+        }
+        return totalPrice;
     }
 }
